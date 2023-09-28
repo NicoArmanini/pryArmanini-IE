b653d2ea7ed1154aa6299d40c928d0c951565a1e
Nico Armanini <armanininico@gmail.com>
2023-09-28 11:00:24 -0300

    ?

---
+++ clsLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryArmanini_IE
+{
+    internal class clsLogin
+    {
+        //Guardamos datos del usuario actual
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        //procedimiento validar usuario y contraseña
+        public static bool Login(string usuario, string contraseña)
+        {
+            string archivoUsuarios = "usuarios.txt";
+
+            if (File.Exists(archivoUsuarios))
+            {
+                using (StreamReader sr = new StreamReader(archivoUsuarios))
+                {
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        string[] partes = linea.Split(':');
+
+                        if (partes.Length == 3)
+                        {
+                            string usuarioArchivo = partes[0];
+                            string contraseñaArchivo = partes[1];
+
+                            if (usuario == usuarioArchivo && contraseña == contraseñaArchivo)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+
+        }
+        public static void RegisterLog(string usuario)
+        {
+            StreamWriter sw = new StreamWriter("logInicio.txt", true);
+            sw.WriteLine(usuario + " - Fecha: " + DateTime.Now);
+            sw.Close();
+        }
+    }
+}
--- clsRegistroProveedor.cs
+++ clsRegistroProveedor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,50 @@ namespace pryArmanini_IE
 {
     internal class clsRegistroProveedor
     {
-        public void Registrar(DataGridView data, ComboBox c)
+        public void Registrar(Int32 id, string entidad, DateTime apertura, int expediente, string juzg, string juri, string direccion, string liquidador)
         {
-            
-        }
+            string archivoProveedor = "Listado de aseguradores.csv";
 
-        
+            try
+            {
+                // Crear una lista para almacenar los IDs existentes en el archivo
+                List<int> idsExistentes = new List<int>();
+
+                using (StreamReader sr = new StreamReader(archivoProveedor))
+                {
+                    string readLine;
+                    while ((readLine = sr.ReadLine()) != null)
+                    {
+                        string[] separador = readLine.Split(';');
+
+                        // Verificar si se pueden convertir los elementos de la línea a los tipos adecuados
+                        if (separador.Length >= 1 && int.TryParse(separador[0], out int existingID))
+                        {
+                                // Agregar el ID a la lista de IDs existentes
+                        idsExistentes.Add(existingID);
+                        }
+                    }
+                }
+
+                // Verificar si el ID ya existe en la lista de IDs existentes
+                if (!idsExistentes.Contains(id))
+                {
+                    string nuevoRegistro = $"{id};{entidad};{apertura};{expediente};{juzg};{juri};{direccion};{liquidador}";
+
+                    using (StreamWriter sw = new StreamWriter(archivoProveedor, true))
+                    {
+                        sw.WriteLine(nuevoRegistro);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El ID ya existe en el archivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar el archivo: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }   
     }
 }
